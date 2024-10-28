01b78e58effb571864d179f89a288c10ca16b0a5
rileyballard6 <rileyrb01@gmail.com>
2024-10-28 15:50:28 -0600

    creating basic game logic, window, and drawing components on screen, add a keyhandler class

--- src/main/java/core/GamePanel.java
+++ src/main/java/core/GamePanel.java
@@ -3,7 +3,7 @@ package core;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int tileSize = 16;
     final int scale = 3;
 
@@ -13,9 +13,48 @@ public class GamePanel extends JPanel {
     final int screenWidth = gameTileSize * maxScreenCol;
     final int screenHeight = gameTileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    //Instantiates the thread object with the gamePanel object
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    //Core of the game. Updates information and Draws info on screen
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+            System.out.println("Test Running");
+
+            update();
+
+            repaint();
+        }
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+        Graphics2D graphics2D = (Graphics2D)graphics;
+
+        graphics2D.setColor(Color.blue);
+
+        graphics2D.fillRect(100, 100, gameTileSize, gameTileSize);
+
+        graphics2D.dispose();
+
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,17 +5,15 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         JFrame frame = new JFrame();
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
         GamePanel gamePanel = new GamePanel();
         frame.add(gamePanel);
 
         frame.pack();
-
         frame.setTitle("My game");
     }
 }
